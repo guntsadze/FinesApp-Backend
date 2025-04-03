d68deaf9a0b5f33aeea42ba13ea3d04e6363a817
guntsadze <dimitri.guncadze@gmail.com>
2025-04-03 14:23:45 +0400

    update fetch parking

--- src/utils/puppeteerUtils.js
+++ src/utils/puppeteerUtils.js
@@ -76,21 +76,20 @@ export const getFinesFromPoliceGe = async (vehicleNo, documentNo) => {
 // ფუნქცია შეჩერებისათვის
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+import { webkit } from "playwright";
+
 export const fetchParkingFines = async (vehicle) => {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
+  const browser = await webkit.launch({ headless: true });
   const page = await browser.newPage();
 
   try {
     await page.goto("https://parking.tbilisi.gov.ge/fines?isTransit=false");
-    await new Promise((resolve) => setTimeout(resolve, 7000));
+    await page.waitForTimeout(7000);
 
     const finesInfo = [];
 
-    await page.type("#mat-input-0", vehicle.vehicleNo);
-    await page.type("#mat-input-1", vehicle.companyCode);
+    await page.fill("#mat-input-0", vehicle.vehicleNo);
+    await page.fill("#mat-input-1", vehicle.companyCode);
 
     await page.waitForSelector("button.mat-raised-button:not([disabled])", {
       timeout: 3000,
@@ -98,17 +97,12 @@ export const fetchParkingFines = async (vehicle) => {
 
     await page.click("button.mat-raised-button:not([disabled])");
 
-    // ველოდებით ტაბლოს გამოჩენას, მაგრამ მივიტანთ try-catch, რომ გავაგრძელოთ მუშაობა
     try {
       await page.waitForSelector(".mat-cell.cdk-column-fineNo", {
         timeout: 5000,
       });
 
-      // const fines = await page.locator(".mat-cell.cdk-column-fineNo").all();
-      const fines = await page.$$eval(
-        ".mat-cell.cdk-column-fineNo",
-        (elements) => elements.map((el) => el.textContent.trim())
-      );
+      const fines = await page.locator(".mat-cell.cdk-column-fineNo").all();
 
       for (let i = 0; i < fines.length; i++) {
         const fineNumber = await fines[i].textContent();
